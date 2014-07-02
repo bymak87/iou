9405959910dac903eb71ec933a6d071ff0d4dca2
Amy Bakameyer <bymak87@gmail.com>
2014-07-02 17:53:17 -0500

    added cellphone, first name and last name to registration

--- iou/config/routes.rb
+++ iou/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   resources :users, only:[:show] do
     resources :events, only:[:create, :show, :index, :delete, :new]

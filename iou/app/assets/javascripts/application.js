5c543e3c30e961e5421c23644628d3f272a97e42
Amy Bakameyer <bymak87@gmail.com>
2014-07-08 14:44:23 -0500

    fixed topbar so it is responsive on small screens

--- iou/app/assets/javascripts/application.js
+++ iou/app/assets/javascripts/application.js
@@ -21,4 +21,13 @@
 
 $(function(){ $(document).foundation(); });
 
+$("#myList li").hide();
+$("#myList li").each(function(i, li) {
+    var $list = $(this).closest('ul');
+    $list.queue(function() {
+        $(li).addClass('animated rollIn').show();
+        $list.dequeue();
+    }).delay(500);
+});
+
 

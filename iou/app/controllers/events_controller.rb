c85cb74dff84421c7929f63556b880bcece93916
Amy Bakameyer <bymak87@gmail.com>
2014-07-09 17:25:39 -0500

    changed button layout

--- iou/app/controllers/events_controller.rb
+++ iou/app/controllers/events_controller.rb
@@ -30,11 +30,27 @@ class EventsController < ApplicationController
 
   end
 
+  def remind
+    account_sid = ENV['SID']
+    auth_token = ENV['TOKEN']
+
+    # set up a client to talk to the Twilio REST API
+    @client = Twilio::REST::Client.new account_sid, auth_token
+
+    #send text
+    @client.account.messages.create(
+    :from => '+18329812216',
+    :to => '+15124847144', #current_user.cellphone
+    :body => 'IOU Event with ... been created'
+    )
+  end
+
   def paid
     @id = params[:event_id]
     @event = Event.find(@id)
     @event.status = "paid"
     @event.save
+
     account_sid = ENV['SID']
     auth_token = ENV['TOKEN']
 
